bce409f8d053b377db5903ea17f4829e03ca8b1d
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2022-01-03 08:36:52 +0800

    train codebert

---
+++ src/train.py
@@ -0,0 +1,135 @@
+## Source
+## - https://huggingface.co/microsoft/codebert-base
+## - https://github.com/microsoft/CodeBERT
+## - https://www.thepythoncode.com/article/finetuning-bert-using-huggingface-transformers-python
+
+import pandas as pd
+from sklearn.metrics import accuracy_score
+import torch
+from transformers.file_utils import is_tf_available, is_torch_available, is_torch_tpu_available
+from transformers import AutoTokenizer, AutoModel
+from transformers import Trainer, TrainingArguments
+import numpy as np
+import random
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.model_selection import train_test_split
+
+
+def set_seed(seed: int):
+    """
+    Helper function for reproducible behavior to set the seed in ``random``, ``numpy``, ``torch`` and/or ``tf`` (if
+    installed).
+
+    Args:
+        seed (:obj:`int`): The seed to set.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    if is_torch_available():
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        # ^^ safe to call this function even if cuda is not available
+
+
+def read_actionable_warning_dataset():
+    target_names = ["open", "closed"]
+    df_train = pd.read_csv("../data-derived/train.csv")
+    df_test = pd.read_csv("../data-derived/test.csv")
+
+    df_train["label"] = df_train["category"].apply(lambda x: 0 if "open" else 1)
+    df_test["label"] = df_test["category"].apply(lambda x: 0 if "open" else 1)
+
+    return (list(df_train["method_content"]), list(df_test["method_content"]), list(df_train["label"]),  list(df_test["label"])), target_names
+    
+
+
+class ActionableWarningDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {k: torch.tensor(v[idx]) for k, v in self.encodings.items()}
+        item["label"] = torch.tensor([self.labels[idx]])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    # calculate accuracy using sklearn's function
+    acc = accuracy_score(labels, preds)
+    return {
+        'accuracy': acc,
+    }
+
+
+if __name__ == "__main__":
+    set_seed(1)
+
+    # the model we gonna train, microsoft/codebert-base
+    model_name = "microsoft/codebert-base"
+
+    # max sequence length for each snippet code sample
+    max_length = 512
+
+    # call the function
+    (train_texts, valid_texts, train_labels, valid_labels), target_names = read_actionable_warning_dataset()
+
+    # load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name)
+
+    # tokenize the dataset, truncate when passed `max_length`,
+    # and pad with 0's when less than `max_length`
+    train_encodings = tokenizer(
+        train_texts, truncation=True, padding=True, max_length=max_length)
+    valid_encodings = tokenizer(
+        valid_texts, truncation=True, padding=True, max_length=max_length)
+
+    # convert our tokenized data into a torch Dataset
+    train_dataset = ActionableWarningDataset(train_encodings, train_labels)
+    valid_dataset = ActionableWarningDataset(valid_encodings, valid_labels)
+
+    # load the model and pass to CUDA
+    model = AutoModel.from_pretrained(
+        model_name, num_labels=len(target_names)).to("cuda")
+
+    training_args = TrainingArguments(
+        output_dir='./results',          # output directory
+        num_train_epochs=3,              # total number of training epochs
+        per_device_train_batch_size=4,  # batch size per device during training
+        per_device_eval_batch_size=4,   # batch size for evaluation
+        warmup_steps=500,                # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,               # strength of weight decay
+        logging_dir='./logs',            # directory for storing logs
+        # load the best model when finished training (default metric is loss)
+        load_best_model_at_end=True,
+        # but you can specify `metric_for_best_model` argument to change to accuracy or other metric
+        logging_steps=400,               # log & save weights each logging_steps
+        save_steps=400,
+        evaluation_strategy="steps",     # evaluate each `logging_steps`
+    )
+
+    trainer = Trainer(
+        model=model,                         # the instantiated Transformers model to be trained
+        args=training_args,                  # training arguments, defined above
+        train_dataset=train_dataset,         # training dataset
+        eval_dataset=valid_dataset,          # evaluation dataset
+        # the callback that computes metrics of interest
+        compute_metrics=compute_metrics,
+    )
+
+    # train the model
+    trainer.train()
+
+    # evaluate the current model after training
+    trainer.evaluate()
+
+    # saving the fine tuned model & tokenizer
+    model_path = "codebert-AI4SE"
+    model.save_pretrained(model_path)
+    tokenizer.save_pretrained(model_path)
